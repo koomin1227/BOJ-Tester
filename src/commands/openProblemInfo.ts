c576ecc0f9cf0d1d8428d4339d052fddffb80a8f
Koo Min <koomin1227@naver.com>
2025-01-20 20:40:25 +0900

    Refactor : panel 을 다루는 로직을 ProblemInfoPanel 로 변경

--- src/commands/openProblemInfo.ts
+++ src/commands/openProblemInfo.ts
@@ -1,80 +1,10 @@
 import * as vscode from 'vscode';
-import { parseProlem } from '../utils/problemParser';
-import { createProblemInfoPanel, getWebviewContent } from '../panels/problemInfoPanel';
-import { Problem } from '../types';
-
-let currentPanel: vscode.WebviewPanel | undefined = undefined;
-let currentProblem: Problem | undefined = undefined;
-export let currentOpendFile: string | undefined = undefined;
+import { ProblemInfoPanel } from '../panels/problemInfoPanel';
 
 export async function openProblemInfo(context: vscode.ExtensionContext) {
-    if (currentPanel) {
-        const currentOpenedProblemId = getCurrentOpenedProblemId();
-
-        if (currentProblem !== undefined && currentOpenedProblemId === null) {
-            vscode.window.showWarningMessage('열려있는 문제 번호 파일이 없습니다.');
-        } 
-        else if (currentProblem !== undefined && currentOpenedProblemId !== null) {
-            if (currentProblem.id !== currentOpenedProblemId) {
-                currentOpendFile = getCurrentOpenedFile();
-                currentProblem = await parseProlem(currentOpenedProblemId);
-                currentPanel.webview.html = getWebviewContent(currentProblem);
-            }
-            currentPanel.reveal(vscode.ViewColumn.Beside);
-        }
+    if (ProblemInfoPanel.currentPanel) {
+        await ProblemInfoPanel.currentPanel.show();
     } else {
-        const currentOpenedProblemId = getCurrentOpenedProblemId();
-        if (currentOpenedProblemId === null) {
-            vscode.window.showWarningMessage('열려있는 문제 번호 파일이 없습니다.');
-            return;
-        }
-        currentOpendFile = getCurrentOpenedFile();
-        currentProblem = await parseProlem(currentOpenedProblemId);
-        currentPanel = createProblemInfoPanel(currentProblem, context);
-
-        currentPanel.onDidDispose(
-            () => {
-                currentPanel = undefined;
-                currentProblem = undefined;
-                currentOpendFile = undefined;
-            },
-            null,
-            context.subscriptions
-        );
+        await ProblemInfoPanel.create(context.extensionUri);
     }
-
 }
-
-function getCurrentOpenedProblemId(): number | null {
-    const activeEditor = vscode.window.activeTextEditor;
-    let problemId = null;
-    if (activeEditor) {
-        let filePath = activeEditor.document.fileName; // 전체 경로
-        const fileName = extractFileName(filePath);
-        if (isNumber(fileName)) {
-            problemId = Number(fileName);
-        }
-    }
-    return problemId;
-}
-
-function getCurrentOpenedFile() {
-    const activeEditor = vscode.window.activeTextEditor;
-    let file = undefined;
-    if (activeEditor) {
-        file = activeEditor.document.fileName; // 전체 경로
-    
-    }
-    return file;
-}
-
-function extractFileName(filePath: string) {
-    const fileNameWithExtension = filePath.split("/").pop() || "";
-    const fileName = fileNameWithExtension.split(".").slice(0, -1).join(".");
-    return fileName;
-}
-
-function isNumber(fileName: string) {
-    return /^\d+$/.test(fileName);
-}
-
--- src/panels/problemInfoPanel.ts
+++ src/panels/problemInfoPanel.ts
@@ -1,278 +1,172 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { Problem, ProblemStats } from "../types";
-import { currentOpendFile } from '../commands/openProblemInfo';
+import { getCurrentOpenedFile, getCurrentOpenedProblemId } from '../utils/fileParser';
+import { parseProlem } from '../utils/problemParser';
 
-export function createProblemInfoPanel(problem: Problem, context: vscode.ExtensionContext) {
-    const panel = vscode.window.createWebviewPanel(
-        'BOJ Tester',
-        `문제`,
-        vscode.ViewColumn.Beside,
-        {
-            enableScripts: true
+export class ProblemInfoPanel {
+    public static currentPanel: ProblemInfoPanel | undefined;
+    public static currentProblem: Problem | undefined = undefined;
+    public static currentOpendFile: string | undefined = undefined;
+	public static readonly viewType = 'BOJ Tester';
+
+	private readonly _panel: vscode.WebviewPanel;
+	private readonly _extensionUri: vscode.Uri;
+	private _disposables: vscode.Disposable[] = [];
+
+    public static async create(extensionUri: vscode.Uri) {
+        const currentOpenedProblemId = getCurrentOpenedProblemId();
+        if (currentOpenedProblemId === null) {
+            vscode.window.showWarningMessage('열려있는 문제 번호 파일이 없습니다.');
+            return;
         }
-    );
-
-    panel.webview.html = getWebviewContent(problem);
-
-    panel.webview.onDidReceiveMessage(
-        async (message) => {
-            if (message.command === 'copySourceCode') {
-                if (currentOpendFile !== undefined) {
-                    const fileContent = fs.readFileSync(currentOpendFile, 'utf-8');
-                    await vscode.env.clipboard.writeText(fileContent);
-                } 
+        ProblemInfoPanel.currentOpendFile = getCurrentOpenedFile();
+        ProblemInfoPanel.currentProblem = await parseProlem(currentOpenedProblemId);
+        const panel = vscode.window.createWebviewPanel(
+            ProblemInfoPanel.viewType,
+            `문제`,
+            vscode.ViewColumn.Beside,
+            {
+                enableScripts: true
             }
-        },
-        undefined,
-        context.subscriptions
-    );
-    return panel;
-}
-
-export function getWebviewContent(problem: Problem) {
-    return `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>${problem.id}</title>
-        ${CSS}
-    </head>
-    <body>
-        <a href="https://www.acmicpc.net/problem/${problem.id}">
-            <h1>${problem.id} - ${problem.title}</h1>
-        </a>
-        <a href="https://www.acmicpc.net/submit/${problem.id}">
-            <button class="submit">제출하기</button>
-        </a>
-        ${getProblemStatsTable(problem.problemStats)}
-        <h2>문제</h2>
-        ${problem.description}
-        <h2>입력</h2>
-        ${problem.inputDiscription}
-        <h2>출력</h2>
-        ${problem.outputDescription}
-        <h2>테스트 케이스</h2>
-        ${getTestCases(problem.inputs, problem.outputs)}
-        <a href="https://www.acmicpc.net/submit/${problem.id}">
-            <button class="submit">제출하기</button>
-        </a>
-        ${SCRIPT}
-        </body>
-    </html>
-    `;
-}
-
-function getProblemStatsTable(problemStats: ProblemStats) {
-    return `
-        <table class="problem-stats-table">
-            <thead>
-                <tr>
-                    <th>시간 제한</th>
-                    <th>메모리 제한</th>
-                    <th>제출</th>
-                    <th>정답</th>
-                    <th>맞힌 사람</th>
-                    <th>정답 비율</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>${problemStats.timeLimit}</td>
-                    <td>${problemStats.memoryLimit}</td>
-                    <td>${problemStats.submissions}</td>
-                    <td>${problemStats.correctSubmissions}</td>
-                    <td>${problemStats.solvers}</td>
-                    <td>${problemStats.correctRate} %</td>
-                </tr>
-            </tbody>
-        </table>
-    `;
-}
-
-function getTestCases(inputs: string[], outputs: string[]) {
-    let html = `<div>`;
-    for (let i = 0; i < inputs.length; i++) {
-        html = html + `<div class="example">
-                    <div class="input">
-                        <h3>입력 ${i + 1}</h3>
-                        <pre>${inputs[i]}</pre>
-                    </div>
-                    <div class="output">
-                        <h3>출력 ${i + 1}</h3>
-                        <pre>${outputs[i]}</pre>
-                    </div>
-                </div>
-                `;
+        );
+        ProblemInfoPanel.currentPanel = new ProblemInfoPanel(panel, extensionUri);
     }
-    return html + '</div>';
-}
 
-const SCRIPT = `
-        <script>
-            const vscode = acquireVsCodeApi();
-            
-            document.querySelector('.submit').addEventListener('click', () => {
-                vscode.postMessage({ command: 'copySourceCode' });
-            });
+    public async show() {
+        const currentOpenedProblemId = getCurrentOpenedProblemId();
 
-            window.addEventListener('message', event => {
-                const message = event.data;
-                if (message.command === 'showNotification') {
-                    alert(message.text);
+        if (ProblemInfoPanel.currentProblem !== undefined && currentOpenedProblemId === null) {
+            vscode.window.showWarningMessage('열려있는 문제 번호 파일이 없습니다.');
+        } 
+        else if (ProblemInfoPanel.currentProblem !== undefined && currentOpenedProblemId !== null) {
+            if (ProblemInfoPanel.currentProblem.id !== currentOpenedProblemId) {
+                ProblemInfoPanel.currentOpendFile = getCurrentOpenedFile();
+                ProblemInfoPanel.currentProblem = await parseProlem(currentOpenedProblemId);
+                ProblemInfoPanel.currentPanel!._panel.webview.html = this.getWebviewContent(ProblemInfoPanel.currentProblem);
+            }
+            ProblemInfoPanel.currentPanel!._panel.reveal(vscode.ViewColumn.Beside);
+        }
+    }
+
+    private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+		this._panel = panel;
+		this._extensionUri = extensionUri;
+        this._panel.webview.html = this.getWebviewContent(ProblemInfoPanel.currentProblem!);
+		this._panel.onDidDispose(() => this.dispose(), null, this._disposables);
+
+
+        panel.webview.onDidReceiveMessage(
+            async (message) => {
+                if (message.command === 'copySourceCode') {
+                    if (ProblemInfoPanel.currentOpendFile !== undefined) {
+                        const fileContent = fs.readFileSync(ProblemInfoPanel.currentOpendFile, 'utf-8');
+                        await vscode.env.clipboard.writeText(fileContent);
+                    } 
+                } else if (message.command === 'copyInput') {
+                    await vscode.env.clipboard.writeText(ProblemInfoPanel.currentProblem?.inputs[message.target]!);
+                } else if (message.command === 'copyOutput') {
+                    await vscode.env.clipboard.writeText(ProblemInfoPanel.currentProblem?.outputs[message.target]!);
                 }
-            });
-        </script>
-`;
+            },
+            undefined,
+            this._disposables
+        );
+	}
 
-const CSS = `
-    <style>
-        :root {
-            --container-padding: 20px;
-            --input-padding-vertical: 6px;
-            --input-padding-horizontal: 4px;
-            --input-margin-vertical: 4px;
-            --input-margin-horizontal: 0;
+    public dispose() {
+        ProblemInfoPanel.currentPanel = undefined;
+    }
+
+    private getWebviewContent(problem: Problem): string {
+        const webview = this._panel.webview;
+		const stylesPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'styles.css');
+        const scriptPathMainPath = vscode.Uri.joinPath(this._extensionUri, 'media', 'script.js');
+
+		const stylesMainUri = webview.asWebviewUri(stylesPathMainPath);
+        const scriptMainUri = webview.asWebviewUri(scriptPathMainPath);
+        
+        return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>${problem.id}</title>
+            <link href="${stylesMainUri}" rel="stylesheet">
+            
+        </head>
+        <body>
+            <a href="https://www.acmicpc.net/problem/${problem.id}">
+                <h1>${problem.id} - ${problem.title}</h1>
+            </a>
+            <a href="https://www.acmicpc.net/submit/${problem.id}">
+                <button class="submit">제출하기</button>
+            </a>
+            ${this.getProblemStatsTable(problem.problemStats)}
+            <h2>문제</h2>
+            ${problem.description}
+            <h2>입력</h2>
+            ${problem.inputDiscription}
+            <h2>출력</h2>
+            ${problem.outputDescription}
+            <h2>테스트 케이스</h2>
+            ${this.getTestCases(problem.inputs, problem.outputs)}
+            <a href="https://www.acmicpc.net/submit/${problem.id}">
+                <button class="submit">제출하기</button>
+            </a>
+            <script src="${scriptMainUri}"></script>
+            </body>
+        </html>
+        `;
+    }
+
+    private getProblemStatsTable(problemStats: ProblemStats) {
+        return `
+            <table class="problem-stats-table">
+                <thead>
+                    <tr>
+                        <th>시간 제한</th>
+                        <th>메모리 제한</th>
+                        <th>제출</th>
+                        <th>정답</th>
+                        <th>맞힌 사람</th>
+                        <th>정답 비율</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>${problemStats.timeLimit}</td>
+                        <td>${problemStats.memoryLimit}</td>
+                        <td>${problemStats.submissions}</td>
+                        <td>${problemStats.correctSubmissions}</td>
+                        <td>${problemStats.solvers}</td>
+                        <td>${problemStats.correctRate} %</td>
+                    </tr>
+                </tbody>
+            </table>
+        `;
+    }
+
+    private getTestCases(inputs: string[], outputs: string[]) {
+        let html = `<div>`;
+        for (let i = 0; i < inputs.length; i++) {
+            html = html + `<div class="example">
+                        <div class="input">
+                            <div class="input-head">
+                                <h3>입력 ${i + 1}</h3>
+                                <button class="input-copy-btn" data-target=${i}>복사</button>
+                            </div>
+                            <pre>${inputs[i]}</pre>
+                        </div>
+                        <div class="output">
+                            <h3>출력 ${i + 1}</h3>
+                            <button class="input-copy-btn" data-target=${i}>복사</button>
+                            <pre>${outputs[i]}</pre>
+                        </div>
+                    </div>
+                    `;
         }
-
-        body {
-            padding: 0 var(--container-padding);
-            color: var(--vscode-foreground);
-            font-size: var(--vscode-font-size);
-            font-weight: var(--vscode-font-weight);
-            font-family: var(--vscode-font-family);
-            background-color: var(--vscode-editor-background);
-        }
-        button {
-            border: none;
-            padding: var(--input-padding-vertical) var(--input-padding-horizontal);
-            width: 100%;
-            text-align: center;
-            outline: 1px solid transparent;
-            outline-offset: 2px !important;
-            color: var(--vscode-button-foreground);
-            background: var(--vscode-button-background);
-        }
-
-        button:hover {
-            cursor: pointer;
-            background: var(--vscode-button-hoverBackground);
-        }
-
-        button:focus {
-            outline-color: var(--vscode-focusBorder);
-        }
-
-        button.secondary {
-            color: var(--vscode-button-secondaryForeground);
-            background: var(--vscode-button-secondaryBackground);
-        }
-
-        button.secondary:hover {
-            background: var(--vscode-button-secondaryHoverBackground);
-        }
-
-        a {
-            text-decoration: none;
-            color: inherit;
-        }
-
-        h1 {
-            position: relative;
-            padding-bottom: 10px;
-            margin-bottom: 20px;
-        }
-
-        h1::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100vw;
-            height: 1px;
-            background-color:rgb(87, 87, 87);
-        }
-
-        h2 {
-            position: relative;
-            padding-bottom: 10px;
-            margin-bottom: 20px;
-            display: inline-block;
-            border-bottom: 1px solid #0076C0;
-        }
-
-        h2::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100vw;
-            height: 1px;
-            background-color:rgb(87, 87, 87);
-        }
-
-        h3 {
-            position: relative;
-            padding-bottom: 10px;
-            margin-bottom: 20px;
-            display: inline-block;
-            border-bottom: 1px solid #0076C0;
-        }
-
-        h3::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 100vw;
-            height: 1px;
-            background-color:rgb(87, 87, 87);
-        }
-
-        pre {
-            padding: 10px;
-            background-color:rgb(53, 53, 53);
-        }
-
-        p {
-            font-size: 14px;
-        }
-
-        .problem-stats-table {
-            border-collapse: collapse;
-            margin: 25px 0;
-            width: 100%;
-            text-align: center;
-        }
-
-        .problem-stats-table thead tr {
-            font-weight: bold;
-            border-bottom: 1px solid rgb(134, 134, 134)
-        }
-
-        .problem-stats-table th, .problem-stats-table td {
-            padding: 12px 15px;
-        }
-
-        .example-container {
-            display: flex;
-            flex-direction: column;
-            gap: 20px;
-        }
-
-        .example {
-            display: flex;
-            gap: 20px;
-
-            padding: 10px;
-            border-radius: 5px;
-        }
-
-        .input, .output {
-            flex: 1;
-        }
-
-    </style>
-`;
+        return html + '</div>';
+    }
+}
