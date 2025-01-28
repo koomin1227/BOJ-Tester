9b621f01ab82bfe85d163dd436766b8b5944580e
Koo Min <koomin1227@naver.com>
2025-01-28 22:01:54 +0900

    Feat : Problem 객체에서 테스트 케이스를 삭제하는 함수 구현

--- src/utils/testCaseManager.ts
+++ src/utils/testCaseManager.ts
@@ -4,3 +4,9 @@ function addTestCase(input: string, output: string, problem: Problem) {
     problem.inputs.push(input);
     problem.outputs.push(output);
 }
+
+function deleteTestCase(testCaseIndex: number, problem: Problem) {
+    if (testCaseIndex >= problem.DefaultTestCaseCount) {
+        problem.inputs.splice(testCaseIndex, 1);
+    }
+}
