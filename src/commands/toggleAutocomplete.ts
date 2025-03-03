e6252d1cb806da178df71edf8a0f5550e19793fa
Koo Min <koomin1227@naver.com>
2025-03-03 22:33:17 +0900

    Feat : 오류 표시 기능 비활성화 기능 추가

--- src/commands/toggleAutocomplete.ts
+++ src/commands/toggleAutocomplete.ts
@@ -1,25 +1,28 @@
 import * as vscode from 'vscode';
 
 export async function toggleAutocomplete() {
-    const config = vscode.workspace.getConfiguration('editor');
+    const editorConfig = vscode.workspace.getConfiguration('editor');
+    const problemsConfig = vscode.workspace.getConfiguration('problems');
 
     let isFeatureEnabled = await getFeatureStatus(); 
     if (isFeatureEnabled) {
-        await config.update('quickSuggestions', { other: false, comments: false, strings: false }, vscode.ConfigurationTarget.Global);
-        await config.update('suggestOnTriggerCharacters', false, vscode.ConfigurationTarget.Global);
-        await config.update('codeLens', false, vscode.ConfigurationTarget.Global);
-        await config.update('lightbulb.enabled', false, vscode.ConfigurationTarget.Global);
-        await config.update('hover.enabled', false, vscode.ConfigurationTarget.Global);
-        await config.update('inlineSuggest.enabled', false, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('quickSuggestions', { other: false, comments: false, strings: false }, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('suggestOnTriggerCharacters', false, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('codeLens', false, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('lightbulb.enabled', false, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('hover.enabled', false, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('inlineSuggest.enabled', false, vscode.ConfigurationTarget.Global);
+        await problemsConfig.update('visibility', false, vscode.ConfigurationTarget.Global);
 
         vscode.window.showInformationMessage('자동완성 기능이 꺼졌습니다.');
     } else {
-        await config.update('quickSuggestions', { other: true, comments: true, strings: true }, vscode.ConfigurationTarget.Global);
-        await config.update('suggestOnTriggerCharacters', true, vscode.ConfigurationTarget.Global);
-        await config.update('codeLens', true, vscode.ConfigurationTarget.Global);
-        await config.update('lightbulb.enabled', true, vscode.ConfigurationTarget.Global);
-        await config.update('hover.enabled', true, vscode.ConfigurationTarget.Global);
-        await config.update('inlineSuggest.enabled', true, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('quickSuggestions', { other: true, comments: true, strings: true }, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('suggestOnTriggerCharacters', true, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('codeLens', true, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('lightbulb.enabled', true, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('hover.enabled', true, vscode.ConfigurationTarget.Global);
+        await editorConfig.update('inlineSuggest.enabled', true, vscode.ConfigurationTarget.Global);
+        await problemsConfig.update('visibility', true, vscode.ConfigurationTarget.Global);
 
         vscode.window.showInformationMessage('자동완성 기능이 켜졌습니다.');
     }
@@ -28,12 +31,14 @@ export async function toggleAutocomplete() {
 }
 
 export async function getFeatureStatus(): Promise<boolean> {
-    const config = vscode.workspace.getConfiguration('editor');
+    const editorConfig = vscode.workspace.getConfiguration('editor');
+    const problemsConfig = vscode.workspace.getConfiguration('problems');
 
-    const quickSuggestions = config.get<{ other: boolean; comments: boolean; strings: boolean }>('quickSuggestions', { other: true, comments: true, strings: true });
-    const suggestOnTriggerCharacters = config.get<boolean>('suggestOnTriggerCharacters', true);
-    const codeLens = config.get<boolean>('codeLens', true);
-    const hoverEnabled = config.get<boolean>('hover.enabled', true);
+    const quickSuggestions = editorConfig.get<{ other: boolean; comments: boolean; strings: boolean }>('quickSuggestions', { other: true, comments: true, strings: true });
+    const suggestOnTriggerCharacters = editorConfig.get<boolean>('suggestOnTriggerCharacters', true);
+    const codeLens = editorConfig.get<boolean>('codeLens', true);
+    const hoverEnabled = editorConfig.get<boolean>('hover.enabled', true);
+    const problemVisibility = problemsConfig.get<boolean>('visibility', true);
 
-    return quickSuggestions.other && suggestOnTriggerCharacters && codeLens && hoverEnabled;
+    return quickSuggestions.other && suggestOnTriggerCharacters && codeLens && hoverEnabled && problemVisibility;
 }
